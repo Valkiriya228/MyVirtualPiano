21fc14521cd8f2919160e9316a8f82e22fb8e026
Anastasia Rubsha <ckfcntyf2000@gmail.com>
2019-07-10 02:18:35 +0300

    initial comment

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -10,7 +10,7 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.awt.*;
-import java.util.Scanner;
+
 
 
 public class Main extends Application  {
@@ -27,86 +27,84 @@ public class Main extends Application  {
         primaryStage.setResizable(false);
         primaryStage.show();
 
-  //      Controller controller = new Controller();
 
-  //              scene.setOnKeyPressed(event -> {
-//            switch (event.getCode()) {
-//                case Z:
-//                    controller.pressNote("src/sample/music/Z.wav");
-//                    break;
-//                case X:
-//                    controller.pressNote("src/sample/music/X.wav");
-//                    break;
-//                case C:
-//                    controller.pressNote("src/sample/music/C.wav");
-//                    break;
-//                case V:
-//                    controller.pressNote("src/sample/music/V.wav");
-//                    break;
-//                case B:
-//                    controller.pressNote("src/sample/music/B.wav");
-//                    break;
-//                case A:
-//                    controller.pressNote("src/sample/music/A.wav");
-//                    break;
- //               case S:
-//                    controller.pressNote("src/sample/music/S.wav");
-//                    break;
-//                case D:
-//                    controller.pressNote("src/sample/music/D.wav");
- //                   break;
-//                case L:
-//                    controller.pressNote("src/sample/music/aa.wav");
-//                    break;
-//                case K:
- //                   controller.pressNote("src/sample/music/bb.wav");
- //                   break;
-//                case P:
- //                   controller.pressNote("src/sample/music/cc.wav");
-//                    break;
-//                case M:
- //                   controller.pressNote("src/sample/music/dd.wav");
-//                    break;
- //               case O:
- //                   controller.pressNote("src/sample/music/ee.wav");
- //                   break;
- //               case U:
- //                   controller.pressNote("src/sample/music/ff.wav");
-//                    break;
-//                case N:
-//                    controller.pressNote("src/sample/music/gg.wav");
-//                    break;
-//
-//                case F:
-//                    controller.pressNote("src/sample/music/F.wav");
- //                   break;
- //               case G:
-//                    controller.pressNote("src/sample/music/G.wav");
- //                   break;
- //               case Q:
- //                   controller.pressNote("src/sample/music/Q.wav");
- //                   break;
- //               case W:
- //                   controller.pressNote("src/sample/music/W.wav");
- //                   break;
- //               case E:
-  //                  controller.pressNote("src/sample/music/E.wav");
-   //                 break;
-  //              case R:
-  //                  controller.pressNote("src/sample/music/R.wav");
-  //                  break;
-//                case T:
- //                   controller.pressNote("src/sample/music/X.wav");
-  //                  break;
-   //             case Y:
-    //                controller.pressNote("src/sample/music/S.wav");
-     //               break;
-   //             case H:
-  //                  controller.pressNote("src/sample/music/H.wav");
-  //                  break;
-  //          }
-
-    //    });
+          Controller controller = new Controller();
+               scene.setOnKeyPressed(event -> {
+            switch (event.getCode()) {
+           case Z:
+               controller.pressNote("resources/music/Z.wav");
+               break;
+           case X:
+               controller.pressNote("resources/music/X.wav");
+               break;
+           case C:
+               controller.pressNote("resources/music/C.wav");
+               break;
+           case V:
+               controller.pressNote("resources/music/V.wav");
+               break;
+           case B:
+               controller.pressNote("resources/music/B.wav");
+               break;
+           case A:
+               controller.pressNote("resources/music/A.wav");
+               break;
+           case S:
+               controller.pressNote("resources/music/S.wav");
+               break;
+           case D:
+               controller.pressNote("resources/music/D.wav");
+               break;
+           case L:
+               controller.pressNote("resources/music/aa.wav");
+               break;
+           case K:
+               controller.pressNote("resources/music/bb.wav");
+               break;
+           case P:
+               controller.pressNote("resources/music/cc.wav");
+               break;
+           case M:
+               controller.pressNote("resources/music/dd.wav");
+               break;
+           case O:
+               controller.pressNote("resources/music/ee.wav");
+               break;
+           case U:
+               controller.pressNote("resources/music/ff.wav");
+               break;
+           case N:
+               controller.pressNote("resources/music/gg.wav");
+               break;
+           case F:
+               controller.pressNote("resources/music/F.wav");
+               break;
+           case G:
+               controller.pressNote("resources/music/G.wav");
+               break;
+           case Q:
+               controller.pressNote("resources/music/Q.wav");
+               break;
+           case W:
+               controller.pressNote("resources/music/W.wav");
+               break;
+           case E:
+               controller.pressNote("resources/music/E.wav");
+               break;
+           case R:
+               controller.pressNote("resources/music/R.wav");
+               break;
+           case T:
+               controller.pressNote("resources/music/X.wav");
+               break;
+           case Y:
+               controller.pressNote("resources/music/S.wav");
+               break;
+           case H:
+               controller.pressNote("resources/music/H.wav");
+               break;
+       }
+   });
     }
 
 
